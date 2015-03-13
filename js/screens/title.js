5b59a56c0cc036fc11abe10d6e28484633f722e7
Jackson McGlasson <originalqwerty45@gmail.com>
2015-03-13 08:38:25 -0700

    started changes to title screen

--- js/screens/title.js
+++ js/screens/title.js
@@ -5,31 +5,26 @@ game.TitleScreen = me.ScreenObject.extend({
     onResetEvent: function () {
         me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage("title-screen")), -10);
 
-        me.input.bindKey(me.input.KEY.ENTER, "start");
+
         me.game.world.addChild(new (me.Renderable.extend({
             init: function () {
-                this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                this._super(me.Renderable, 'init', [270, 240, 300, 50]);
                 this.font = new me.Font("Arial", 46, "white");
             },
             draw: function (renderer) {
-                this.font.draw(renderer.getContext(), "AWESOMENAUTS!", 350, 130);
-                this.font.draw(renderer.getContext(), "Press Enter TO Start", 350, 430);
+                this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x, this.pos.y);
+
             }
 
         })));
-        
-        
-        this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
-            if (action === "start") {
-                me.state.change(me.state.PLAY);
-            }
-        });
+
+
+
     },
     /**	
      *  action to perform when leaving this screen (state change)
      */
     onDestroyEvent: function () {
-        me.input.unbindKey(me.input.KEY.ENTER);
-        me.event.unsubscribe(this.handler); 
+
     }
 });
