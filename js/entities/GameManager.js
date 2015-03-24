a566ae7ce83c7bca3d1c082e0d34d908591a8a43
Jackson McGlasson <originalqwerty45@gmail.com>
2015-03-24 08:58:44 -0700

    started spend gold screen

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -49,6 +49,7 @@ game.ExperienceManager = Object.extend({
         } else if (game.data.win === false && !this.gameover) {
             this.gameOver(false);
         }
+        console.log(game.data.exp);
 
         return true;
     },
@@ -64,4 +65,44 @@ game.ExperienceManager = Object.extend({
     }
 });
 
+game.SpendGold = Object.extend({
+    init: function (x, y, settings) {
+        this.now = new Date().getTime();
+        this.lastBuy = new Date().getTime();
+        this.paused = false;
+        this.alwaysUpdate = true;
+        this.updateWhenPaused = true;
+        this.buying = false;
+    },
+    update: function () {
+        this.now = new Date().getTime();
+
+        if (me.input.isKeyPressed("buy") && this.now - this.lastBuy >= 1000) {
+            this.lastBuy = this.now;
+            if (!this.buying) {
+                this.startBuying;
+            } else {
+                this.stopBuying;
+            }
+        }
+        return true;
+    },
+    startBuying: function () {
+       this.buying = true;
+       me.state.pause(me.state.PLAY);
+       game.data.pausePos = me.game.viewport.localToWorld(0, 0);
+       game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage("data/img/spend.png"));
+       game.data.buyscreen.updateWhenPaused = true;
+       game.data.buyscreen.setOpacity(0.8);
+       me.game.world.addChild(game.data.buyscreen, 34);
+       game.data.player.setVelocity(0, 0)
+    },
+    stopBuying: function () {
+       this.buying = false;
+       me.state.resume(me.state.PLAY);
+       game.data.player.setVelocity(game.data.playerMoveSpeed, 20)
+       me.game.world.removeChild(game.data.buyscreen);
+    }
+
+});
 
